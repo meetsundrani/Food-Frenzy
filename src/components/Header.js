fcf62a7fce7d74e35660f46ae6292367d06cbb0f
Meets windows <meetsundrani@gmail.com>
2023-12-27 18:34:32 +0530

    modified the file structure clean the code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import { LOGO_URL } from "../utils/constants"
+
 const Header = () => {
     return (
     <div className="header">
      <div>
-     <img className="logo" src="https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148986823.jpg?w=740&t=st=1703340099~exp=1703340699~hmac=7ff8bd9e441047748a82e9c9d48f5c99941655495c94a0615637d37bc161b8a6"/>
+                <img className="logo" src={LOGO_URL}/>
      </div>
      <div className="nav-items">
      <ul>
--- src/components/RestauranCard.js
+++ src/components/RestauranCard.js
@@ -1,9 +1,10 @@
+import { CON_URL } from "../utils/constants"
 const RestaurantCard = (props) => {
     const { resData } = props;
     const { name, cloudinaryImageId, cuisines, avgRatingString, sla, costForTwo } = resData?.info;
     return (
         <div className="res-card">
-            <img className="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId}></img>
+            <img className="res-logo" src={CON_URL + cloudinaryImageId}></img>
             <h3>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRatingString} stars</h4>
