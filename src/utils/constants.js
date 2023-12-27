4abe823ddec07b0b8a6db9a0e5b6cb41605f879b
Meets windows <meetsundrani@gmail.com>
2023-12-27 18:38:52 +0530

    added mockData folder

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,2 @@
-const LOGO_URL = "https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148986823.jpg?w=740&t=st=1703340099~exp=1703340699~hmac=7ff8bd9e441047748a82e9c9d48f5c99941655495c94a0615637d37bc161b8a6";
-const CON_URL ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
-
-export {CON_URL, LOGO_URL}
+export const LOGO_URL = "https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148986823.jpg?w=740&t=st=1703340099~exp=1703340699~hmac=7ff8bd9e441047748a82e9c9d48f5c99941655495c94a0615637d37bc161b8a6";
+export const CON_URL ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
