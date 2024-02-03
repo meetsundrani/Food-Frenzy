41f634b54e9a913b370574c478dad811b7ac56cf
Meets windows <meetsundrani@gmail.com>
2024-02-03 19:45:52 +0530

    useEffect called on dependency changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LOGO_URL } from "../utils/constants"
 
 const Header = () => {
     const [btnName, setBtnName] = useState("Login");
+    
+    useEffect(() => {
+    }, [btnName]);
     return (
     <div className="header">
      <div>
