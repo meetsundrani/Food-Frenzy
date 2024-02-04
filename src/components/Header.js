796b3567eab3b0037f0070b314a05714e4ca299f
Meets windows <meetsundrani@gmail.com>
2024-02-04 20:17:36 +0530

    new hook useRouterError

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,28 +4,28 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     const [btnName, setBtnName] = useState("Login");
-    
+
     useEffect(() => {
     }, [btnName]);
     return (
-    <div className="header">
-     <div>
-                <img className="logo" src={LOGO_URL}/>
-     </div>
-     <div className="nav-items">
-     <ul>
+        <div className="header">
+            <div>
+                <img className="logo" src={LOGO_URL} />
+            </div>
+            <div className="nav-items">
+                <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About us</Link></li>
                     <li><Link to="/contact">Contact us</Link></li>
-     <li>Cart</li>
-     <button className="login" onClick={() => {
-        btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
-     }}>
-     {btnName}
-     </button>
-     </ul>
-     </div>
-    </div>
+                    <li>Cart</li>
+                    <button className="login" onClick={() => {
+                        btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+                    }}>
+                        {btnName}
+                    </button>
+                </ul>
+            </div>
+        </div>
     );
 }
 
